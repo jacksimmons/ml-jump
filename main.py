9db920f3811e7594367eb9846858339682f55eb4
piklman <56202789+piklman@users.noreply.github.com>
2019-11-13 17:14:10 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,65 @@
 import pygame
-from pygame.locals import *
 
-class App:
-    def __init__(self):
-        self._running = True
-        self._display_surf = None
-        self.size = self.weight, self.height = 640, 400
+#Initialise pygame
+pygame.init()
 
-    def on_init(self):
-        pygame.init()
-        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
-        self._running = True
+#Create a display window
 
-    def on_event(self, event):
+wx = 1000 #Display's x dimension
+wy = 1000 #Display's y dimension
+
+splitx = 100 #The x coordinate of the line separating the main window and the dev console
+
+disp = pygame.display.set_mode((wx, wy))
+pygame.display.set_caption("Title Screen")
+
+#Initialise fonts
+font = pygame.font.SysFont('Bahnschrift', 30)
+
+x = 50
+y = 50
+w = 50
+h = 50
+
+v = 5
+
+dim = 60, 40
+
+black = 0, 0, 0
+white = 255, 255, 255
+red = 255, 0, 0
+green = 0, 255, 0
+blue = 0, 0, 255
+
+rects = {
+    'button1': (10,20,30,40) #x, y, w, h
+}
+
+#Create a Clock object and limit framerate
+clock = pygame.time.Clock()
+fps_limit = 60
+
+#Titlescreen loop
+title = True
+while title:
+    clock.tick(fps_limit)
+
+    #Event handling
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            self._running = False
-    def on_loop(self):
-        pass
-    def on_render(self):
-        pass
-    def on_cleanup(self):
-        pygame.quit()
+            title = False
+        if event.type == 
 
-    def on_execute(self):
-        if self.on_init() == False:
-            self._running = False
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_SPACE]:
+        y -= v
 
-        while( self._running ):
-            for event in pygame.event.get():
-                self.on_event(event)
-            self.on_loop()
-            self.on_render()
-        self.on_cleanup()
+    print(clock.get_time())
 
-if __name__ == "__main__" :
-    theApp = App()
-    theApp.on_execute()
+    disp.fill(black)
+    pygame.draw.line(disp, white, (splitx, wy), (splitx, 0))
+    pygame.draw.rect(disp, red, (x, y, w, h))
+    font.render('Text', False, white)
+    pygame.display.update()
+
+pygame.quit()
