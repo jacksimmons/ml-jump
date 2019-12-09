6ca51c1320276a5061b3cebabee107137333c06c
piklman <56202789+piklman@users.noreply.github.com>
2019-12-09 09:53:01 +0000

    added a downwards staircase

--- obstacle_generator.py
+++ obstacle_generator.py
@@ -1,4 +1,4 @@
-﻿import random
+import random
 import math
 import pygame
 
@@ -6,7 +6,7 @@ class ObjectGenerator:
 	def __init__(self, obh):
 		self.obh = obh #The ObjectHandler that instantiated this object
 		self.gen_objs = [] #All of the generated objects currently on screen
-		self.formations = ["Spike", "Dip", "Spike2"] #To make the generation easier to understand
+		self.formations = ["Spike", "Dip", "Staircase"] #To make the generation easier to understand
 
 		#Max player jump height = 109 pixels
 		#Max player jump width = 47 frames * velocity of moving objects
@@ -54,61 +54,86 @@ class ObjectGenerator:
 
 					x = x + (w * 4) #So that the next generated object is after obj4...
 					y = py - (ph + h * 3) #...and in line with it.
+                    
+                for x in range(5): #Create landscape in groups of 5
+                    in_loop = True
+                    if in_loop:
+                        r = random.choice(self.formations)
 
-					r = random.choice(self.formations)
+                        if r == "Spike":
+                            #This is a basic obstacle, which the player must jump over
+                            colour = (255, 0, 0)
+                            gnd = obh.create_object((x, y, w/3, h), colour)
+                            gnd2 = obh.create_object((x + w/3, y, w/3, h), colour)
+                            gnd3 = obh.create_object((x + (w/3 * 2), y, w/3, h), colour)
 
-					if r == "Spike":
-						colour = (255, 0, 0)
-						gnd = obh.create_object((x, y, w/3, h), colour)
-						gnd2 = obh.create_object((x + w/3, y, w/3, h), colour)
-						gnd3 = obh.create_object((x + (w/3 * 2), y, w/3, h), colour)
+                            spike = obh.create_object((x + w/3, y - h, w/3, h), colour)
+                            self.ao(gnd, True, True, True)
+                            self.ao(gnd2, True, True, True)
+                            self.ao(gnd3, True, True, True)
+                            self.ao(spike, True, False, True)
 
-						spike = obh.create_object((x + w/3, y - h, w/3, h), colour)
-						self.ao(gnd, True, True, True)
-						self.ao(gnd2, True, True, True)
-						self.ao(gnd3, True, True, True)
-						self.ao(spike, True, False, True)
+                            self.gen_objs.append(gnd)
+                            self.gen_objs.append(gnd2)
+                            self.gen_objs.append(gnd3)
+                            self.gen_objs.append(spike)
 
-						self.gen_objs.append(gnd)
-						self.gen_objs.append(gnd2)
-						self.gen_objs.append(gnd3)
-						self.gen_objs.append(spike)
+                            x = x + (w * 3) #For most formations, y displacement = 0.
 
-						x = x + (w * 3) #For most formations, y displacement = 0.
+                        elif r == "Dip":
+                            #This is a 'Dip' where the player must go down and then back up
+                            colour = (0, 255, 0)
+                            #Where y = starting y
+                            gnd = obh.create_object((x, y, w, h), colour)
+                            gnd2 = obh.create_object((x + (w * 6), y, w, h), colour)
+                            dip = obh.create_object((x + (w * 3), y, w, h), colour)
 
-					elif r == "Dip":
-						colour = (0, 255, 0)
-						#Where y = starting y
-						gnd = obh.create_object((x, y, w, h), colour)
-						gnd2 = obh.create_object((x + (w * 6), y, w, h), colour)
-						dip = obh.create_object((x + (w * 3), y, w, h), colour)
+                            #Where y < starting y and is decreasing:
+                            gndd1 = obh.create_object((x + w, y + h, w, h), colour)
+                            gndd2 = obh.create_object((x + (w * 2), y + (h * 2), w, h), colour)
+                            gndd3 = obh.create_object((x + (w * 3), y + (h * 3), w, h), colour)
 
-						#Where y < starting y and is decreasing:
-						gndd1 = obh.create_object((x + w, y + h, w, h), colour)
-						gndd2 = obh.create_object((x + (w * 2), y + (h * 2), w, h), colour)
-						gndd3 = obh.create_object((x + (w * 3), y + (h * 3), w, h), colour)
+                            #Where y < starting y but is increasing:
+                            gndu1 = obh.create_object((x + (w * 4), y + (h * 2), w, h), colour)
+                            gndu2 = obh.create_object((x + (w * 5), y + h, w, h), colour)
 
-						#Where y < starting y but is increasing:
-						gndu1 = obh.create_object((x + (w * 4), y + (h * 2), w, h), colour)
-						gndu2 = obh.create_object((x + (w * 5), y + h, w, h), colour)
+                            self.ao(gnd, True, True, True)
+                            self.gen_objs.append(gnd)
+                            self.ao(gnd2, True, True, True)
+                            self.gen_objs.append(gnd2)
+                            self.ao(dip, True, False, True)
+                            self.gen_objs.append(dip)
 
-						self.ao(gnd, True, True, True)
-						self.gen_objs.append(gnd)
-						self.ao(gnd2, True, True, True)
-						self.gen_objs.append(gnd2)
-						self.ao(dip, True, False, True)
-						self.gen_objs.append(dip)
+                            self.ao(gndd1, True, True, True)
+                            self.gen_objs.append(gndd1)
+                            self.ao(gndd2, True, True, True)
+                            self.gen_objs.append(gndd2)
+                            self.ao(gndd3, True, True, True)
+                            self.gen_objs.append(gndd3)
 
-						self.ao(gndd1, True, True, True)
-						self.gen_objs.append(gndd1)
-						self.ao(gndd2, True, True, True)
-						self.gen_objs.append(gndd2)
-						self.ao(gndd3, True, True, True)
-						self.gen_objs.append(gndd3)
+                            self.ao(gndu1, True, True, True)
+                            self.gen_objs.append(gndu1)
+                            self.ao(gndu2, True, True, True)
+                            self.gen_objs.append(gndu2)
 
-						self.ao(gndu1, True, True, True)
-						self.gen_objs.append(gndu1)
-						self.ao(gndu2, True, True, True)
-						self.gen_objs.append(gndu2)
+                            x = x + (w * 7)
 
-						x = x + (w * 7)
+                        elif r == "Staircase":
+                            #This is a downwards staircase
+                            colour = (0, 0, 255)
+                            obj = obh.create_object((x, y, w, h), colour)
+                            obj2 = obh.create_object((x + w, y + h, w, h), colour)
+                            obj3 = obh.create_object((x + (w*2), y + (h*2), w, h), colour)
+                            obj4 = obh.create_object((x + (w*3), y + (h*3), w, h), colour)
+
+                            self.gen_objs = [obj, obj2, obj3, obj4]
+
+                            self.ao(obj, True, True, True)
+                            self.ao(obj2, True, True, True)
+                            self.ao(obj3, True, True, True)
+                            self.ao(obj4, True, True, True)
+
+                            x = x + (w * 4) #So that the next generated object is after obj4...
+                            in_loop = False
+                    else:
+                        break
